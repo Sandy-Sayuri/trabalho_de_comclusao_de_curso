c31fde23f3d138f5279663774b4975e00527cfd8
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-08-09 23:46:32 -0300

    refatorando

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,35 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { BoardUserComponent } from './board-user/board-user.component';
-import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { MaterialModule } from './material.module';
+import { getPaginatorIntl } from './paginator';
+import { LoginService } from './shared/services/login.service';
+import { LoginComponent } from './pages/login/login.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    ProfileComponent,
-    BoardAdminComponent,
-    BoardModeratorComponent,
-    BoardUserComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    BrowserAnimationsModule, 
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [authInterceptorProviders],
+  providers: [ LoginService,
+    {provide: MatPaginatorIntl, useValue: getPaginatorIntl()}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
