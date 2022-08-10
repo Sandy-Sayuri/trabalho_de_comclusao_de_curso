8f18dd0c291260b9251f7ddc12e538276c0305ca
sandy <ssishii@stefanini.com>
2022-08-10 14:16:30 -0300

    okkk

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,15 +10,14 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { getPaginatorIntl } from './paginator';
 import { LoginService } from './shared/services/login.service';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './view/login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-
+    LoginComponent
   ],
   imports: [
     BrowserModule,
