f9bf69ef5cc97c9ee8874d270992df9c1a523e98
sandy <ssishii@stefanini.com>
2022-08-12 16:38:55 -0300

    login quase finalizado

--- front-1/src/app/app.module.ts
+++ front-1/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
 import { getPaginatorIntl } from './paginator';
@@ -21,7 +22,8 @@ import { LoginComponent } from './view/login/login.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
