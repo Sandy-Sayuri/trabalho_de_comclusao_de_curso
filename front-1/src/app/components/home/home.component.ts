49cd819d568d21180cad129b91be7626f42fdb17
sandy <ssishii@stefanini.com>
2022-08-17 16:01:15 -0300

    atualização

--- front-1/src/app/components/home/home.component.ts
+++ front-1/src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-teste',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class testeComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 
   constructor() { }
 
