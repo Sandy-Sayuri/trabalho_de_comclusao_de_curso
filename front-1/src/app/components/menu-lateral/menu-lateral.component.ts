7932af7c0e325e926bb0cf3696299d1b5275b58f
sandy <ssishii@stefanini.com>
2022-10-08 17:50:28 -0300

    deito o refrex

--- front-1/src/app/components/menu-lateral/menu-lateral.component.ts
+++ front-1/src/app/components/menu-lateral/menu-lateral.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from 'src/app/shared/services/login.service';
 import { LoginComponent } from 'src/app/view/login/login.component';
@@ -12,20 +13,26 @@ export class MenuLateralComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter();
   usuario:string
   pontuacao:number
-  constructor(public LoginService: LoginService) { }
+  constructor(public LoginService: LoginService,
+    private router: Router,) { }
   id:number
   estilo:false
   ngOnInit() {
     this.id=this.LoginService.dados
-    this.LoginService.users(this.id).subscribe({ 
-      next: (retorno:any)=>{
-        this.usuario=retorno.name
-        this.pontuacao=retorno.team.total
-      }
-    })
-
-
+    if(this.id==undefined){
+      this.router.navigateByUrl('/login')
+    }else{
+      this.LoginService.users(this.id).subscribe({ 
+        next: (retorno:any)=>{
+          this.usuario=retorno.name
+          this.pontuacao=retorno.team.total
+        }
+      })
+     
+    }
+    
   }
+
  
   public onSidenavClose = (estilo:boolean) => {
     this.sidenavClose.emit();
