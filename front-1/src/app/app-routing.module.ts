52d47bd6379bcb311edcb7e81b5d6572a5e52cc8
sandy <ssishii@latam.stefanini.com>
2022-10-22 13:35:12 -0300

    tela jogador

--- front-1/src/app/app-routing.module.ts
+++ front-1/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './view/cadastro/cadastro.component';
 import { HomeComponent } from './view/home/home.component';
+import { JogadorComponent } from './view/jogador/jogador.component';
 import { LoginComponent } from './view/login/login.component';
 import { Ranking } from './view/ranking/ranking.component';
 import { UsersComponent } from './view/users/users.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path:'ranking',component:Ranking},
   {path:'cadastro',component:CadastroComponent},
   {path:'usuario',component:UsersComponent},
+  {path:'jogadora',component:JogadorComponent},
   {path:'**',redirectTo:'login'}
 ];
 
