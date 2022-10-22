8049ef4b1117314f6c5277256e210ab13ee5c0f3
sandy <ssishii@latam.stefanini.com>
2022-10-22 02:55:47 -0300

    falta apenas o formulario

--- front-1/src/app/app.module.ts
+++ front-1/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CadastreComponent } from './components/cadastro/cadastro.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { testeComponent } from './components/home/home.component';
+import { JogadoresComponent } from './components/jogadores/jogadores.component';
 
 import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -21,11 +22,13 @@ import { UserComponent } from './components/user/user.component';
 import { MaterialModule } from './material.module';
 import { CadastroService } from './shared/services/cadastro.service';
 import { HomeService } from './shared/services/home.service';
+import { JogadoresService } from './shared/services/jogadores.service';
 import { LoginService } from './shared/services/login.service';
 import { MenuService } from './shared/services/menu.service';
 import { UsersService } from './shared/services/users.service';
 import { CadastroComponent } from './view/cadastro/cadastro.component';
 import { HomeComponent } from './view/home/home.component';
+import { JogadorComponent } from './view/jogador/jogador.component';
 import { LoginComponent } from './view/login/login.component';
 import { AuthInterceptor } from './view/login/login.interceptor';
 import { Ranking } from './view/ranking/ranking.component';
@@ -49,7 +52,9 @@ import { UsersComponent } from './view/users/users.component';
     Ranking,
     RankingComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    JogadoresComponent,
+    JogadorComponent
 
   ],
   imports: [
@@ -67,6 +72,7 @@ import { UsersComponent } from './view/users/users.component';
     CadastroService,
     MenuService,
     UsersService,
+    JogadoresService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     {provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ],
