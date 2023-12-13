2c38c52f644f7d8d4166ad2d7bf0b6a1e2ff63a4
nome <ssishii@latam.stefanini.com>
2023-12-13 11:52:11 -0300

    correção

--- front-1/src/app/shared/services/common.service.ts
+++ front-1/src/app/shared/services/common.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Store } from '@ngrx/store';
 import * as fromApp from '@app/app.reducer';
+import { Store } from '@ngrx/store';
 import * as LOADING from '@shared/store/loading/loading.actions';
-import * as FileSaver from 'file-saver';
-import * as xlsx from 'xlsx';
 import * as crypto from 'crypto-js';
 
 @Injectable({
@@ -13,14 +11,6 @@ import * as crypto from 'crypto-js';
 export class CommonServices {
 	constructor(private store: Store<fromApp.AppState>) {}
 				
-	babel(value: string): string{
-		let babel: any[] = JSON.parse(this.decrypt(localStorage.getItem('@virtual-assistant:babel')));
-		let translate = babel.find(val => val.name.toUpperCase() == value.toUpperCase()) ? babel.find(val => val.name.toUpperCase() == value.toUpperCase()).translate : null
-		if(translate==null)
-			console.log("** BABEL ** Sem tradução: " + value);
-			
-		return translate == null ? 'BABEL - Sem tradução!' : translate;
-	}
 
 	//* Função que formata o objeto para o padrão Base64
 	convertToBase64(item) {
@@ -34,22 +24,6 @@ export class CommonServices {
 	decrypt(val:any): string{
 		var bytes  = crypto.AES.decrypt(val, 'zxc#fdsaFDAwsj;6');
 		return bytes.toString(crypto.enc.Utf8);
-	}
-
-	//função que transforma as informações da tabela para o formato xls
-	public exportExcel(jsonData: any[], fileName: string): void {
-		const worksheet = xlsx.utils.json_to_sheet(jsonData);
-		const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-		const excelBuffer: any = xlsx.write(workbook, {
-			bookType: 'xlsx',
-			type: 'array',
-		});
-		this.saveExcelFile(excelBuffer, fileName);
-	}
-
-	getTheme(): string{
-	return localStorage.getItem('@virtual-assistant:config')?
-		JSON.parse(localStorage.getItem('@virtual-assistant:config')).theme:'light'
 	}
 
 	numberOnly(event): boolean {
@@ -81,25 +55,4 @@ export class CommonServices {
 		}
 	}
 
-	//função que salva as informações da tabela para o formato xls
-	private saveExcelFile(buffer: any, fileName: string): void {
-		let EXCEL_TYPE =
-			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-		let EXCEL_EXTENSION = '.xlsx';
-		let date = new Date();
-		let format =
-			'  ' + date.getDate() + ' - ' + date.getHours() + '_' + date.getMinutes();
-		const data: Blob = new Blob([buffer], {
-			type: EXCEL_TYPE,
-		});
-		FileSaver.saveAs(data, fileName + format + EXCEL_EXTENSION);
-	}
-
-	translateArray(array: any[]): any[]{
-		let arrayFinished: any[]=[]
-		array.forEach(element => {			          
-			arrayFinished.push({name: this.babel(element.name), code: element.id })
-		});
-		return arrayFinished
-	}
 }
